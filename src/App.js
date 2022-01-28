e34c9a62c7b37e28382661feebaa9af3a5b56a55
unknown <ashuisalive@gmail.com>
2022-01-29 00:51:28 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import FeaturedGames from './components/FeaturedGames';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import Newswire from './components/Newswire';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Newswire />
       <FeaturedGames />
       <Footer/>
