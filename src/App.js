55a8f88be09c385b56c56658c89d77aa5338ecf0
unknown <ashuisalive@gmail.com>
2022-01-29 00:33:33 +0530

    footer completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import FeaturedGames from './components/FeaturedGames';
+import Footer from './components/Footer';
 import Newswire from './components/Newswire';
 
 function App() {
   return (
     <div className="App">
       <Newswire />
-      <FeaturedGames/>
+      <FeaturedGames />
+      <Footer/>
     </div>
   );
 }
