3ea95c6fb20d02f9afb8b072e8accbc93545d6c6
unknown <ashuisalive@gmail.com>
2022-01-30 00:22:29 +0530

    minor changes to crousal design

--- src/components/Crousal.jsx
+++ src/components/Crousal.jsx
@@ -13,9 +13,9 @@ const Crousal = () => {
                     <div className='h1' style={{paddingTop: '15%', marginLeft: '10%', marginBottom: '50%'}}><Button style={{padding: '5%', width: '65%', fontWeight: '500', fontSize: '20px'}} variant='outlined' className='text-white border-light'>WATCH NOW</Button></div>
                     <div className='d-flex justify-content-evenly align-items-center'>
                         <div style={{backgroundColor: 'white', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
-                        <div style={{backgroundColor: 'white', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
-                        <div style={{backgroundColor: 'white', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
-                        <div style={{backgroundColor: 'white', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
+                        <div style={{backgroundColor: 'rgb(102,102,102)', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
+                        <div style={{backgroundColor: 'rgb(102,102,102)', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
+                        <div style={{backgroundColor: 'rgb(102,102,102)', height: '0.5vh', width: '20%', borderRadius: '20px'}}></div>
                     </div>
                 </div>
             </>
